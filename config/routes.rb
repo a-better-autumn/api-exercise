c95033a593d081b2b69c85a6e05362cb6226350e
Autumn <abetterautumn@gmail.com>
2017-10-11 21:22:59 +0800

    generate api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, :default => { :format => :json } do
+    namespace :v1 do
+      get "/trains" => "trains#index", :as => :trains
+      get "/trains/:train_number" => "trains#show", :as => :train
+      post "/reservations" => "reservations#create", :as => :create_reservations
+      get "/reservations/:booking_code" => "reservations#show", :as => :reservation
+      patch "reservations/:booking_code" => "reservations#update", :as => :update_reservation
+      delete "reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
+    end
+  end
+
   resources :cities do
     member do
       post :update_temp
